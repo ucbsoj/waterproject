3d70abad09985dc109d49310b2489640a64d0f19
Jeremy Rue <jrue@calpress>
2014-04-25 12:06:45 -0700

    update to min.'s

--- js/main.js
+++ js/main.js
@@ -10,34 +10,3 @@
 
 // //On document ready
 // $(document).ready(VimeoEmbed.init);
-
-
-
-jQuery(document).ready(function($){
-
-	var w_height = $(window).height();
-	var w_width  = $(window).width();
-
-
-
-	$('#opening_video').css({height : w_height, width : (w_height * 16 / 9)});
-
-
-
-
-
-	});
-
-	console.log($('vimeo iframe').css('width'));
-
-
-
-	// $('iframe').each(function(iframe, index){
-
-	// 	if($(this).attr('src').match(/vimeo/)){
-
-	// 	}
-
-	// })
-
-});
