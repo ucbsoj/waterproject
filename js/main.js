4aca6c22b036d01dfd8f3f6aabca2b956387c775
Jeremy Rue <jrue@calpress>
2014-04-25 12:04:07 -0700

    added prefix-free js, and centered opening video

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-var VimeoEmbed = {};
+// var VimeoEmbed = {};
 
-//Called on document ready
-VimeoEmbed.init = function(e) {
-    //Listen to the load event for all the iframes on the page
-    $('iframe').each(function(index, iframe) {
-        iframe.addEvent('onLoad', VimeoEmbed.vimeo_player_loaded);
-    });
-};
+// //Called on document ready
+// VimeoEmbed.init = function(e) {
+//     //Listen to the load event for all the iframes on the page
+//     $('iframe').each(function(index, iframe) {
+//         iframe.addEvent('onLoad', VimeoEmbed.vimeo_player_loaded);
+//     });
+// };
 
-//On document ready
-$(document).ready(VimeoEmbed.init);
+// //On document ready
+// $(document).ready(VimeoEmbed.init);
+
+
+
+jQuery(document).ready(function($){
+
+	var w_height = $(window).height();
+	var w_width  = $(window).width();
+
+
+
+	$('#opening_video').css({height : w_height, width : (w_height * 16 / 9)});
+
+
+
+
+
+	});
+
+	console.log($('vimeo iframe').css('width'));
+
+
+
+	// $('iframe').each(function(iframe, index){
+
+	// 	if($(this).attr('src').match(/vimeo/)){
+
+	// 	}
+
+	// })
+
+});
